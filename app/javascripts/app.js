d21b7d1a5bedf3d575b052facf87b7867a233836
Peter Ince <peter@impactlabs.xyz>
2018-10-03 20:03:42 +1000

    complete first version of three party demo

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -8,12 +8,14 @@ import { default as contract } from 'truffle-contract'
 import { default as BigNumber } from 'bignumber.js'
 
 // Import our contract artifacts and turn them into usable abstractions.
-import maintoken_artifacts from '../../build/contracts/MainToken.json'
-import regtoken_artifacts from '../../build/contracts/RegionalToken.json'
+import maintokenArtifacts from '../../build/contracts/MainToken.json'
+import rentisTokenArtifacts from '../../build/contracts/RentisToken.json'
+import insurelyTokenArtifacts from '../../build/contracts/InsurelyToken.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-const MainToken = contract(maintoken_artifacts)
-const RegToken = contract(regtoken_artifacts)
+const MainToken = contract(maintokenArtifacts)
+const RegToken = contract(rentisTokenArtifacts)
+const InsToken = contract(insurelyTokenArtifacts)
 
 // The following code is simple to show off interacting with your contracts.
 // As your needs grow you will likely need to change its form and structure.
@@ -21,18 +23,23 @@ const RegToken = contract(regtoken_artifacts)
 let accounts
 let account
 let regAccounts
+let insAccounts
 let web3Main = new Web3()
 let web3Reg = new Web3()
+let web3Ins = new Web3()
 
-web3Main.setProvider(new web3Main.providers.HttpProvider('http://127.0.0.1:8545'))
-web3Reg.setProvider(new web3Reg.providers.HttpProvider('http://127.0.0.1:22000'))
+web3Main.setProvider(new web3Main.providers.HttpProvider('http://127.0.0.1:9545'))
+web3Reg.setProvider(new web3Reg.providers.HttpProvider('http://127.0.0.1:9545'))
+web3Ins.setProvider(new web3Ins.providers.HttpProvider('http://127.0.0.1:9545'))
+
 window.App = {
 
   start: function () {
     // Bootstrap the MetaCoin abstraction for Use.
     MainToken.setProvider(web3Main.currentProvider)
     RegToken.setProvider(web3Reg.currentProvider)
-    // Get the i nitial account balance so it can be displayed.
+    InsToken.setProvider(web3Ins.currentProvider)
+    // Get the initial account balance so it can be displayed.
     web3Main.eth.getAccounts(function (err, accs) {
       if (err != null) {
         window.alert('There was an error fetching your accounts.')
@@ -40,7 +47,7 @@ window.App = {
       }
 
       if (accs.length === 0) {
-        window.alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.")
+        window.alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
         return
       }
 
@@ -56,19 +63,35 @@ window.App = {
       }
 
       if (accs.length === 0) {
-        window.alert("Couldn't get any accounts! Make sure your Quorum client is configured correctly.")
+        window.alert('Couldn\'t get any accounts! Make sure your Quorum client is configured correctly.')
         return
       }
 
       regAccounts = accs
     })
+
+    // get the accounts from quorum
+    web3Ins.eth.getAccounts(function (err, accs) {
+      if (err != null) {
+        window.alert('There was an error fetching your accounts.')
+        return
+      }
+
+      if (accs.length === 0) {
+        window.alert('Couldn\'t get any accounts! Make sure your Quorum client is configured correctly.')
+        return
+      }
+
+      insAccounts = accs
+    })
+
     MainToken.deployed().then(function (f) {
       let mtContract = f
-      mtContract.totalSupply().then(function(val) {
-        $('#mainTotal').text(val.toString());
+      mtContract.totalSupply().then(function (val) {
+        $('#mainTotal').text(val.toString())
       })
-      mtContract.availableSupply().then(function(val) {
-        $('#mainAvailable').text(val.toString());
+      mtContract.availableSupply().then(function (val) {
+        $('#mainAvailable').text(val.toString())
       })
       let tLock = mtContract.TokenLock({}, { fromBlock: 0, toBlock: 'latest' })
       tLock.watch(function (error, result) {
@@ -92,14 +115,13 @@ window.App = {
     })
     RegToken.deployed().then(function (f) {
       const regContract = f
-      regContract.totalSupply().then(function(val) {
-        $('#regTotal').text(val.toString());
+      regContract.totalSupply().then(function (val) {
+        $('#regTotal').text(val.toString())
       })
       let mint = regContract.Mint({}, { fromBlock: 0, toBlock: 'latest' })
       mint.watch(function (error, result) {
         if (!error) {
-          $('#regLoader').hide()
-          $('#regional').append(`<li class="collection-item">${result.args.amount} tokens have been minted</li>`)
+          $('#main').append(`<li class="collection-item">${result.args.amount} Rentis Rewards Tokens have been minted</li>`)
         } else {
           console.log(error)
         }
@@ -108,8 +130,7 @@ window.App = {
       let burn = regContract.Burn({}, { fromBlock: 0, toBlock: 'latest' })
       burn.watch(function (error, result) {
         if (!error) {
-          $('#regLoader').hide()
-          $('#regional').append(`<li class="collection-item">${result.args.value} tokens have been burned</li>`)
+          $('#main').append(`<li class="collection-item">${result.args.value} Rentis Rewards Tokens have been burned</li>`)
         } else {
           console.log(error)
         }
@@ -118,79 +139,145 @@ window.App = {
       let mintFin = regContract.MintFinished({}, { fromBlock: 0, toBlock: 'latest' })
       mintFin.watch(function (error, result) {
         if (!error) {
-          $('#regLoader').hide()
-          $('#regional').append(`Minting has finished\n`)
+          $('#main').append(`Rentis minting has finished\n`)
+        } else {
+          console.log(error)
+        }
+      })
+    })
+    InsToken.deployed().then(function (f) {
+      const insContract = f
+      insContract.totalSupply().then(function (val) {
+        $('#insTotal').text(val.toString())
+      })
+      let mint = insContract.Mint({}, { fromBlock: 0, toBlock: 'latest' })
+      mint.watch(function (error, result) {
+        if (!error) {
+          $('#main').append(`<li class="collection-item">${result.args.amount} Insurely Loyalty Tokens have been minted</li>`)
+        } else {
+          console.log(error)
+        }
+      })
+
+      let burn = insContract.Burn({}, { fromBlock: 0, toBlock: 'latest' })
+      burn.watch(function (error, result) {
+        if (!error) {
+          $('#main').append(`<li class="collection-item">${result.args.value} Insurely Loyalty Tokens have been burned</li>`)
+        } else {
+          console.log(error)
+        }
+      })
+
+      let mintFin = insContract.MintFinished({}, { fromBlock: 0, toBlock: 'latest' })
+      mintFin.watch(function (error, result) {
+        if (!error) {
+          $('#main').append(`Insurely minting has finished\n`)
         } else {
           console.log(error)
         }
       })
     })
   },
-  allocateTokens : function (tokenAmount) {
+  allocateTokens: function (tokenAmount, allocNet) {
     const amount = new BigNumber(tokenAmount)
     MainToken.deployed().then(function (main) {
-      main.availableSupply().then(function(val) {
+      main.availableSupply().then(function (val) {
         let available = val.minus(amount)
-        $('#mainAvailable').text(available.toString());
+        $('#mainAvailable').text(available.toString())
       })
-      return main.lock(tokenAmount, {from: account})
+      return main.lock(tokenAmount, { from: account })
     }).then(function (res) {
       console.log('Lock successful')
-    }).catch(function(e) {
+    }).catch(function (e) {
       console.log(e)
     })
 
-    RegToken.deployed().then(function (main) {
-      main.totalSupply().then(function(val) {
-        let total = val.plus(amount)
-        $('#regTotal').text(total.toString());
+    if (allocNet === 'rentis') {
+
+      RegToken.deployed().then(function (main) {
+        main.totalSupply().then(function (val) {
+          let total = val.plus(amount)
+          $('#regTotal').text(total.toString())
+        })
+        return main.mint(regAccounts[0].valueOf(), tokenAmount, { from: regAccounts[0] })
+      }).then(function (res) {
+        console.log('Mint successful')
+        $('#revTokenAmount').val("")
+      }).catch(function (e) {
+        console.log(e)
       })
-      return main.mint(regAccounts[0].valueOf(), tokenAmount, {from: regAccounts[0]})
-    }).then(function (res) {
-      console.log('Mint successful')
-    }).catch(function(e) {
-      console.log(e)
-    })
-  },
-  deallocate : function (tokenAmount) {
+    } else {
+      InsToken.deployed().then(function (main) {
+        main.totalSupply().then(function (val) {
+          let total = val.plus(amount)
+          $('#insTotal').text(total.toString())
+        })
+        return main.mint(insAccounts[0].valueOf(), tokenAmount, { from: insAccounts[0] })
+      }).then(function (res) {
+        console.log('Mint successful')
+        $('#insTokenAmount').val("")
+      }).catch(function (e) {
+        console.log(e)
+      })
+    }
+  }
+  ,
+  deallocate: function (tokenAmount, deallocNet) {
+
     const amount = new BigNumber(tokenAmount)
     MainToken.deployed().then(function (main) {
-      main.availableSupply().then(function(val) {
+      main.availableSupply().then(function (val) {
         let available = val.plus(amount)
-        $('#mainAvailable').text(available.toString());
+        $('#mainAvailable').text(available.toString())
       })
-      return main.unlock(tokenAmount, {from: account})
+      return main.unlock(tokenAmount, { from: account })
     }).then(function (res) {
       console.log('Unlock successful')
-    }).catch(function(e) {
+    }).catch(function (e) {
       console.log(e)
     })
 
-    RegToken.deployed().then(function (main) {
-      main.totalSupply().then(function(val) {
-        let total = val.minus(amount)
-        $('#regTotal').text(total.toString());
+    if (deallocNet === 'rentis') {
+      RegToken.deployed().then(function (main) {
+        main.totalSupply().then(function (val) {
+          let total = val.minus(amount)
+          $('#regTotal').text(total.toString())
+        })
+        return main.burn(tokenAmount, { from: regAccounts[0] })
+      }).then(function (res) {
+        console.log('Burn successful')
+        $('#revTokenAmount').val("")
+      }).catch(function (e) {
+        console.log(e)
       })
-      return main.burn(tokenAmount, {from: regAccounts[0]})
-    }).then(function (res) {
-      console.log('Burn successful')
-    }).catch(function(e) {
-      console.log(e)
-    })
+    } else {
+      InsToken.deployed().then(function (main) {
+        main.totalSupply().then(function (val) {
+          let total = val.minus(amount)
+          $('#insTotal').text(total.toString())
+        })
+        return main.burn(tokenAmount, { from: insAccounts[0] })
+      }).then(function (res) {
+        console.log('Burn successful')
+        $('#insTokenAmount').val("")
+      }).catch(function (e) {
+        console.log(e)
+      })
+    }
   }
 }
 
 window.addEventListener('load', function () {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
 // Is there is an injected web3 instance?
-if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source like Metamask")
+  if (typeof web3 !== 'undefined') {
+    console.warn('Using web3 detected from external source like Metamask')
     // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
+    web3Main = new Web3(web3.currentProvider)
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    console.warn('No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    //window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
   }
   App.start()
 })
--- truffle.js
+++ truffle.js
@@ -5,14 +5,14 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 8545,
+      port: 9545,
       network_id: '*', // Match any network id
       gas: 4500000
     },
     rinkeby: {
         host: '127.0.0.1',
         port: 8545,
-        network_id: '4', 
+        network_id: '4',
         gas: 4612388,
         //gasPrice: 1,
         from: '0xf01679f083a9539c7fa5271ac7aa67731f78a0af'
